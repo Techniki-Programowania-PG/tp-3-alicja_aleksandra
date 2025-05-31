4b26bbaec1923aa9777c3aff800e78bce258fbb7
Aleksandra Stosio <olka.stosio@gmail.com>
2025-05-31 15:30:27 +0200

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+from ._core import __doc__, __version__, plot_signal, generate_sin_signal, generate_cos_signal, \
+      generate_square_signal, generate_pilo_signal, filter_1d, filter_2d, dft, idft, interpolacja
+
+__all__ = ["__doc__", "__version__", "plot_signal", "generate_sin_signal", 
+           "generate_cos_signal", "generate_square_signal", 
+           "generate_pilo_signal", "filter_1d", "filter_2D", "dft", "idft", "interpolacja"]
